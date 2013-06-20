9e1ad66d6f895c13b29efa4bd251a11232d8df83
Strand McCutchen <strand@nird.us>
2013-06-20 11:33:48 -0700

    Creates CoursesStudents migration and model

--- db/migrate/20130620181757_create_courses_students.rb
+++ db/migrate/20130620181757_create_courses_students.rb
@@ -1,8 +1,8 @@
 class CreateCoursesStudents < ActiveRecord::Migration
   def change
     create_table :courses_students do |table|
-      table.student_id,     :integer
-      table.course_id,      :integer
+      table.integer :student_id
+      table.integer :course_id
 
       table.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620174535) do
+ActiveRecord::Schema.define(:version => 20130620181757) do
 
   create_table "courses", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(:version => 20130620174535) do
     t.integer  "student_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "courses_students", :force => true do |t|
+    t.integer  "student_id"
+    t.integer  "course_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "students", :force => true do |t|
